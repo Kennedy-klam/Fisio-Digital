75cd6c695f15ee913b187ca0edb189683dbd7720
Kennedy Marques <kennedy.klam@gmail.com>
2024-09-22 18:59:48 -0300

    Teste Cadastro de Paciente DB

---
+++ TestePaciente/conexao.php
@@ -0,0 +1,14 @@
+<?php 
+$dbHost = "localhost";
+$dbUser = "root";
+$dbPass = "";
+$dbName = "testefichasdeavaliação";
+
+$mysqli = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
+
+// if ($mysqli->connect_errno){
+//     echo "Falha ao conectar:";
+// }else{
+//     echo "Conectado";
+// }
+?>
