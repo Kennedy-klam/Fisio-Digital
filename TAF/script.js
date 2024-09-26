2d2f54b3d3d395062d2e7676ce210dae021e0572
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-09-26 07:08:59 -0300

    commit
    atualização de código

--- TAF/script.js
+++ TAF/script.js
@@ -1,15 +1,45 @@
-let minutos = 0;
-let tempo = 0;
-let intervalo;
+let cronometro
+let totalSegundos = 0
+
+function formatarTempo(segundos) {
+    const minutos = Math.floor(segundos / 60)
+    const segRestantes = segundos % 60
+    return `${minutos < 10 ? '0' : ''}${minutos}:${segRestantes < 10 ? '0' : ''}${segRestantes}`
+}
 
 function comecar() {
-  intervalo = setInterval(() => {
-    tempo++;
-    const minutos = Math.floor(tempo / 60).toString().padStart(2, '0');
-    const segundos = (tempo % 60).toString().padStart(2, '0');
-    document.getElementById('tempo').textContent = `${minutos}:${segundos}`;
-  }, 1000);
+    parar(); 
+    cronometro = setInterval(() => {
+        totalSegundos++
+        document.getElementById("tempo").innerText = formatarTempo(totalSegundos)
+        mostrarResultado(totalSegundos)
+    }, 1000)
 }
+
 function parar() {
-  clearInterval(intervalo);
+    clearInterval(cronometro)
+}
+
+function resetar() {
+    parar();
+    totalSegundos = 0
+    document.getElementById("tempo").innerText = "00:00"
+    document.getElementById("descricao").innerText = ""
+}
+
+function mostrarResultado(segundos) {
+    const minutos = segundos / 60;
+    let descricao = "";
+
+    if (minutos <= 10 / 60) {
+        descricao = "Desempenho normal para adultos saudáveis. BAIXO RISCO DE QUEDA."
+    } else if (minutos <= 20 / 60) {
+        descricao = "Normal para idosos frágeis ou com deficiência, mas que são independentes para a maioria das atividades de vida diária (AVD's). BAIXO RISCO DE QUEDA."
+    } else if (minutos <= 29 / 60) {
+        descricao = "Avaliação funcional obrigatória. Abordagem específica para prevenção de queda. RISCO DE QUEDAS MODERADO."
+    } else if (minutos >= 30 / 60) {
+        descricao = "30 segundos ou mais. ALTO RISCO DE QUEDAS.";
+    }
+
+    document.getElementById("descricao").innerText = descricao;
 }
