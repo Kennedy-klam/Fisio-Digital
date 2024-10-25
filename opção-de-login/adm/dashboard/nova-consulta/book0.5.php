230c826a67e057aa4edac653c96b75ba4fd3cb83
Laysa-ba <laysaassuncao2015@gmail.com>
2024-10-25 14:42:55 -0300

    banco v4
    book0.5 e book1 funcionando com o novo banco

--- opção-de-login/adm/dashboard/nova-consulta/book0.5.php
+++ opção-de-login/adm/dashboard/nova-consulta/book0.5.php
@@ -9,113 +9,105 @@ if (!isset($_SESSION['id'])) {
 }
 
 // Obtendo o nome do administrador para exibição
-$admid = $_SESSION['id'];
-$query = "SELECT * FROM `loginadm` WHERE admid = $admid";
+$idAdministrador = $_SESSION['id'];
+$query = "SELECT nome FROM administrador WHERE idAdministrador = $idAdministrador";
 $result = mysqli_query($con, $query);
-if ($result && mysqli_num_rows($result) > 0) {
-    $data = mysqli_fetch_assoc($result);
-    $admnome = $data['admnome'];
-    $_SESSION['admnome'] = $admnome; // Adiciona o nome do administrador na sessão
-} else {
-    $admnome = "Administrador"; // Nome padrão se não encontrar no banco
-    $_SESSION['admnome'] = $admnome; // Adiciona o nome padrão na sessão
-}
+$admnome = ($result && mysqli_num_rows($result) > 0) ? mysqli_fetch_assoc($result)['nome'] : "Administrador";
+$_SESSION['nome'] = $admnome;
 
 // Verificando se o paciente foi selecionado
-if (isset($_POST['selectpatient']) && isset($_POST['clientid'])) {
-    $_SESSION['clientid'] = $_POST['clientid'];  // Salva o ID do paciente na sessão
-    header("Location: book1.php");  // Redireciona para a próxima página
+if (isset($_POST['selectpatient']) && isset($_POST['idPaciente'])) {
+    $_SESSION['idPaciente'] = $_POST['idPaciente'];
+    header("Location: book1.php");
     exit();
 }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Procurar Paciente</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 
 <body>
     <div class="container-fluid pt-2 pb-2" style="background-color: #113838">
         <div class="row">
             <div class="col-sm-10">
-                <img width="200" class="ml-0 mt-2 mb-2" src="../../../../imagens/default_transparent.png">
+                <img width="200" class="ml-0 mt-2 mb-2" src="../../../../imagens/default_transparent.png" alt="Logo">
             </div>
             <div class="col-sm-2">
-                <h3 class="mr-0 text-white">Olá, <?php echo $admnome; ?>!</h3>
+                <h3 class="text-white">Olá, <?= $admnome ?>!</h3>
                 <a href="../dashboard.php" class="btn btn-outline-light">Painel do Administrador</a>
             </div>
         </div>
     </div>
+    
     <div class="container-fluid text-center" style="background-color:#79e3bd">
-        <div class="">
-            <h2 class="pt-3 pb-3">Procurar Paciente</h2>
-        </div>
+        <h2 class="pt-3 pb-3">Procurar Paciente</h2>
     </div>
 
-    <form action="" class="w-50 m-auto">
+    <form action="" method="GET" class="w-50 m-auto">
         <div class="form-group row">
             <label class="align-self-center m-2">Coloque o Nome</label>
             <input type="text" class="form-control w-50 align-self-center m-2" name="nome_paciente" placeholder="Digite o nome do paciente">
-            <button type="submit" class="btn btn-dark align-self-center m-2" value="Login" name="search">Procurar pacientes</button>
+            <button type="submit" class="btn btn-dark align-self-center m-2" name="search">Procurar pacientes</button>
         </div>
     </form>
+    
     <hr>
     <h2 class="text-center">Pacientes Disponíveis</h2>
     <hr>
-    <?php
-    if (isset($_GET['search']) && !empty($_GET['nome_paciente'])) {
+
+    <?php if (isset($_GET['search']) && !empty($_GET['nome_paciente'])): 
         $nome_paciente = $_GET['nome_paciente'];
-        // Consulta para buscar pacientes pelo nome usando LIKE
-        $sql = "SELECT * FROM loginc WHERE clientname LIKE '%$nome_paciente%'";
+        $sql = "SELECT idPaciente, nome, email, celular, sexo FROM paciente WHERE nome LIKE '%$nome_paciente%'";
         $res = mysqli_query($con, $sql);
-        if ($res) {
-            if (mysqli_num_rows($res)) {
-                echo "<div class='table-responsive text-center'>";
-                echo "<form action='' method='POST'>";  // Alterado para POST
-                echo "<table class='table table-hover'>
-                        <thead>
-                          <tr>
-                            <th scope='col'> </th>
-                            <th scope='col'> ID </th>
-                            <th scope='col'> Nome </th>
-                            <th scope='col'> E-mail </th>
-                            <th scope='col'> Telefone </th>
-                            <th scope='col'> Sexo </th>
-                          </tr>
-                        </thead>";
-
-                while ($row = mysqli_fetch_assoc($res)) {
-                    echo "<tr> 
-                                <td> <input type='radio' name='clientid' value='" . $row['clientid'] . "'></td>
-                                <td scope='row'>" . $row["clientid"] . " </td>
-                                <td>" . $row["clientname"] . "</td>
-                                <td>" . $row["clientemail"] . "</td>
-                                <td>" . $row["phoneno"] . " </td>
-                                <td>" . $row["sex"] . " </td>
-                               </tr>";
-                }
-                echo "</table> <br> <br>";
-                echo " <button type='submit' class='btn btn-dark ml-auto mb-50' value='selectpatient' name='selectpatient'>Selecionar Paciente</button>";
-                echo "</form> </div>";
-            } else {
-                echo "<h5 class='text-center'>Nenhum paciente encontrado.</h5>";
-            }
-        } else {
-            echo "<h5 class='text-center'>Erro na consulta ao banco de dados.</h5>";
-        }
-    } else {
-        echo "<h5 class='text-center'>Digite um nome válido para buscar.</h5>";
-    }
-    mysqli_close($con);
     ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-Df5IicThPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-</body>
+        <?php if ($res && mysqli_num_rows($res) > 0): ?>
+            <div class="table-responsive text-center">
+                <form action="" method="POST">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col"></th>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">E-mail</th>
+                                <th scope="col">Telefone</th>
+                                <th scope="col">Sexo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php while ($row = mysqli_fetch_assoc($res)): ?>
+                                <tr>
+                                    <td><input type="radio" name="idPaciente" value="<?= $row['idPaciente'] ?>"></td>
+                                    <td><?= $row["idPaciente"] ?></td>
+                                    <td><?= $row["nome"] ?></td>
+                                    <td><?= $row["email"] ?></td>
+                                    <td><?= $row["celular"] ?></td>
+                                    <td><?= $row["sexo"] ?></td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                    <button type="submit" class="btn btn-dark mb-3" name="selectpatient">Selecionar Paciente</button>
+                </form>
+            </div>
+        <?php else: ?>
+            <h5 class="text-center">Nenhum paciente encontrado.</h5>
+        <?php endif; ?>
+    <?php else: ?>
+        <h5 class="text-center">Digite um nome válido para buscar.</h5>
+    <?php endif; 
+    mysqli_close($con); 
+    ?>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
 </html>
--- opção-de-login/adm/dashboard/nova-consulta/book1.php
+++ opção-de-login/adm/dashboard/nova-consulta/book1.php
@@ -2,17 +2,15 @@
 session_start();
 require('../../conexões/db.php');
 
-// Verifica se o clientid está na sessão
-if (!isset($_SESSION["clientid"])) {
-    // Redireciona de volta para a seleção de paciente se o clientid não estiver definido
+// Verifica se o ID do paciente está na sessão
+if (!isset($_SESSION["idPaciente"])) {
     header("Location: book0.5.php");
     exit();
 }
 
 // Verifica se o nome do administrador está na sessão
-if (!isset($_SESSION["admnome"])) {
-    // Caso o nome do administrador não esteja na sessão, redireciona ou trata o erro.
-    $_SESSION['admnome'] = "Administrador";
+if (!isset($_SESSION["nome"])) {
+    $_SESSION['nome'] = "Administrador";
 }
 
 // Armazena o clinicid na sessão ao selecionar uma clínica
@@ -22,98 +20,98 @@ if (isset($_GET["selectclinic"])) {
     exit();
 }
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Procurar Clínica</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 
 <body>
     <div class="container-fluid pt-2 pb-2" style="background-color: #113838">
         <div class="row">
             <div class="col-sm-10">
-                <img width="200" class="ml-0 mt-2 mb-2" src="../consultas-marcadas/default_transparent.png">
+                <img width="200" class="ml-0 mt-2 mb-2" src="../consultas-marcadas/default_transparent.png" alt="Logo">
             </div>
             <div class="col-sm-2">
-                <!-- Exibe o nome do administrador -->
-                <h3 class="mr-0 text-white">Olá, <?php echo $_SESSION['admnome']; ?>!</h3>
+                <h3 class="text-white">Olá, <?= $_SESSION['nome'] ?>!</h3>
                 <a href="../dashboard.php" class="btn btn-outline-light">Painel do Administrador</a>
             </div>
         </div>
     </div>
+
     <div class="container-fluid text-center" style="background-color:#79e3bd">
-        <div class="">
-            <h2 class="pt-3 pb-3">Procurar Clínica</h2>
-        </div>
+        <h2 class="pt-3 pb-3">Procurar Clínica</h2>
     </div>
 
     <form action="" class="w-50 m-auto">
         <div class="form-group row">
             <label class="align-self-center m-2">Coloque o local</label>
-            <input type="text" class="form-control w-50 align-self-center m-2" name="location" placeholder="Digite o local da clínica">
-            <button type="submit" class="btn btn-dark align-self-center m-2" value="Login" name="search">Procurar Clínicas</button>
+            <input type="text" class="form-control w-50 align-self-center m-2" name="localização" placeholder="Digite o local da clínica">
+            <button type="submit" class="btn btn-dark align-self-center m-2" name="search">Procurar Clínicas</button>
         </div>
     </form>
+    
     <hr>
     <h2 class="text-center">Clínicas Disponíveis</h2>
     <hr>
+
     <?php
-    if (isset($_GET['search']) && !empty($_GET['location'])) {
-        $loc = $_GET['location'];
-        $sql = "SELECT * FROM cliniclogin WHERE location='$loc'";
+    if (isset($_GET['search']) && !empty($_GET['localização'])) {
+        $loc = mysqli_real_escape_string($con, $_GET['localização']);
+        $sql = "SELECT * FROM clinica WHERE localização LIKE '%$loc%'";
         $res = mysqli_query($con, $sql);
+        
         if ($res) {
-            if (mysqli_num_rows($res)) {
+            if (mysqli_num_rows($res) > 0) {
                 echo "<div class='table-responsive text-center'>";
-                echo "<form action='' method='GET'>";  // Alterado para POST
+                echo "<form action='' method='GET'>";
                 echo "<table class='table table-hover'>
                         <thead>
                           <tr>
-                            <th scope='col'> </th>
-                            <th scope='col'> ID </th>
-                            <th scope='col'> Nome </th>
-                            <th scope='col'> E-mail</th>
-                            <th scope='col'> Número de Celular</th> 
-                            <th scope='col'> Localização </th>
-                            <th scope='col'> Endereço </th>
-                            <th scope='col'> Dono </th>
+                            <th></th>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>Telefone</th> 
+                            <th>Localização</th>
+                            <th>Endereço</th>
                           </tr>
-                          </thead>";
+                        </thead>";
 
                 while ($row = mysqli_fetch_assoc($res)) {
                     echo "<tr> 
-                                <td> <input type='radio' name='clinicid' value='" . $row['clinicid'] . "'></td>
-                                <td scope='row'>" . $row["clinicid"] . " </td>
-                                <td>" . $row["clinicname"] . "</td>
-                                <td>" . $row["clinicemail"] . "</td>
-                                <td>" . $row["phoneno"] . " </td>
-                                <td>" . $row["location"] . " </td>
-                                <td>" . $row["address"] . "</td>
-                                <td>" . $row["owner"] . " </td>
-                               </tr> ";
+                            <td><input type='radio' name='clinicid' value='" . $row['idClinica'] . "'></td>
+                            <td>" . $row["idClinica"] . "</td>
+                            <td>" . $row["nome"] . "</td>
+                            <td>" . $row["email"] . "</td>
+                            <td>" . $row["telefone"] . "</td>
+                            <td>" . $row["localização"] . "</td>
+                            <td>" . $row["endereço"] . "</td>
+                          </tr>";
                 }
-                echo "</table>  <br> <br>";
-                echo " <button type='submit' class='btn btn-dark ml-auto mb-50' value='selectclinic' name='selectclinic'>Selecionar Clínica</button>";
-                echo "</form> </div>";
+                echo "</table><br><br>";
+                echo "<button type='submit' class='btn btn-dark' name='selectclinic'>Selecionar Clínica</button>";
+                echo "</form></div>";
             } else {
-                echo "<h5 class=text-center>0 Resultados</h5>";
+                echo "<h5 class='text-center'>Nenhuma clínica encontrada.</h5>";
             }
         } else {
-            echo "<h5 class=text-center>Erro ao consultar o banco de dados</h5>";
+            echo "<h5 class='text-center'>Erro ao consultar o banco de dados.</h5>";
         }
     } else {
-        echo "<h5 class=text-center>Digite uma localização válida </h5>";
+        echo "<h5 class='text-center'>Digite uma localização válida.</h5>";
     }
     mysqli_close($con);
     ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-Df5IicThPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
