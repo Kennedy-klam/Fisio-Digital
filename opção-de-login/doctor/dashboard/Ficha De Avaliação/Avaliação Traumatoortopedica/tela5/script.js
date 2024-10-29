375f01848957d52b0a4c503f00b88e0a29401ee6
Samuel Monteiro <smf.monteiro1701@gmail.com>
2024-10-29 12:01:42 -0300

    atualização de código
    implementação de áreas de texto, novas colunas, interação pra ir para a próxima página.

---
+++ opção-de-login/doctor/dashboard/Ficha De Avaliação/Avaliação Traumatoortopedica/tela5/script.js
@@ -0,0 +1,5 @@
+
+//passar pra proxima pagina
+function redirecionar(url){
+    window.location.href = url;
+}
---
+++ opção-de-login/doctor/dashboard/Ficha De Avaliação/Avaliação Traumatoortopedica/tela6/script.js
@@ -0,0 +1,4 @@
+//passar pra próxima página
+function redirecionar(url){
+    window.location.href = url;
+}
